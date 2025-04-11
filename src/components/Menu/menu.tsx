290842d1133b6b881e2744a9258e9b79be3d78d2
joao.nelson@yduqs.com.br <joao.nelson@yduqs.com.br>
2025-04-11 16:52:07 -0300

    Semana 10 ajustes

--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -4,19 +4,19 @@ import { Link } from 'react-router-dom';
 
 import styles from './Menu.module.scss';
 
-const Menu = () => {
+export default function Menu ()  {
   return (
     <nav className={styles.menu}>
       <ul>
         <li><Link to='/'>Home</Link></li>
-        <li><Link to="/cadastro">Cadastro de Usuários</Link></li>
+        <li><Link to="/usuarios/cadastro">Cadastro de Usuários</Link></li>
         <li><Link to="/usuarios">Lista de Usuários</Link></li>
-        <li><Link to='/'>Cadastro de Produtos</Link></li>
-        <li><Link to='/'>Lista de Produtos</Link></li>
+        <li><Link to='/produtos/cadastro'>Cadastro de Produtos</Link></li>
+        <li><Link to='/produtos'>Lista de Produtos</Link></li>
 
 
       </ul>
     </nav>
   );
 };
-export default Menu;
+ 
